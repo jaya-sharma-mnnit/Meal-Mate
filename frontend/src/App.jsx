df1f2f9b79d067b397f2fc0c471bc627b7d5b608
jaya-sharma-mnnit <sjaya211004@gmail.com>
2024-06-24 22:19:53 +0530

    feat: add appDownload component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
+import AppDownload from './components/AppDownload/AppDownload'
 
 const App = () => {
   return (
@@ -19,7 +20,9 @@ const App = () => {
         <Route path='/' element={<PlaceOrder/>}/>
         </Routes>
     </div>
+    <AppDownload/>
     <Footer/>
+    
     </>
   )
 }
